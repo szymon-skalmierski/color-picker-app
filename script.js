47b6c4ad2843660109089963f7480133b0cef53d
szymon-skalmierski <105440298+szymon-skalmierski@users.noreply.github.com>
2023-06-01 02:38:59 +0200

    color picker option

--- script.js
+++ script.js
@@ -2,18 +2,22 @@ const box = document.querySelector('.box');
 const rangesText = document.querySelectorAll('.range span:nth-of-type(2)');
 const rgbResults = document.querySelectorAll('#rgb p span');
 const hexResult = document.querySelector("#hex p");
+const colorInput = document.querySelector(".color-input input");
 let ranges = document.querySelectorAll('.range input');
 
-function rgbToHex(){
+function rgbToHex(source){
     let hex = "#";
-    ranges.forEach((element)=>{
+    source.forEach((element)=>{
         let hexValue = (+(element.value)).toString(16);
         if(hexValue.length > 1) hex += hexValue;
         else hex += `0${hexValue}`;
     })
     hexResult.innerHTML = hex;
+    return hex;
 }
+colorInput.value = rgbToHex(ranges);
 
+console.log(document.body.style.backgroundColor)
 if(sessionStorage.getItem('rgb')){
     let rgb = sessionStorage.getItem('rgb').split(',');
     ranges.forEach((rangeInput, index)=>{
@@ -23,18 +27,36 @@ if(sessionStorage.getItem('rgb')){
 
 box.addEventListener('input', ()=>{
     sessionStorage.setItem('rgb', [ranges[0].value, ranges[1].value, ranges[2].value]);
+});
+colorInput.addEventListener('input', ()=>{
+    if(colorInput.value.length===4){
+        ranges.forEach((val, index)=>{
+            val.value = parseInt(colorInput.value[index+1], 16);
+        })
+    } else {
+        ranges.forEach((val, index)=>{
+            val.value = parseInt(colorInput.value.slice(1+index*2, 1+index*2+2), 16);
+        })
+    }
+    ranges.forEach((element, index)=> {
+        rgbToHex(ranges)
+
+        rgbResults[index].innerHTML = ranges[index].value;
+        rangesText[index].innerHTML = ranges[index].value;
+        document.body.style.backgroundColor = `rgb(${ranges[0].value},${ranges[1].value},${ranges[2].value})`;
+    });
 })
 
 
 document.body.style.backgroundColor = `rgb(${ranges[0].value},${ranges[1].value},${ranges[2].value})`;
 
-rgbToHex()
+rgbToHex(ranges)
 
 ranges.forEach((element, index)=> {
     rgbResults[index].innerHTML = ranges[index].value;
     rangesText[index].innerHTML = ranges[index].value;
     element.addEventListener('input', ()=>{
-        rgbToHex()
+        rgbToHex(ranges)
 
         rgbResults[index].innerHTML = ranges[index].value;
         rangesText[index].innerHTML = ranges[index].value;
